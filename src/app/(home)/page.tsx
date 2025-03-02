6b8b8d4e4606a9712daaaf71d6bf5e68b53d362f
DizzlyD <david4henery00@hotmail.com>
2025-03-02 20:52:34 +0000

    removed-comments-pre-dnd-kit

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,37 +1,4 @@
-// import {
-//   dehydrate,
-//   HydrationBoundary,
-//   QueryClient,
-// } from "@tanstack/react-query";
-// import { fetchLinks } from "@/query/queryFunctions";
-// import { getQueryClient } from "@/query/Providers";
-// import listOptions from "@/query/linksListOptions";
-
-import {
-  // Navbar,
-  LinksSection,
-  // ProfileSection,
-  // MobilePreviewSection,
-  SaveButton,
-} from "@/components";
-
-// import { redirect } from "next/navigation";
-// import { createClient } from "@/utils/server";
-// import { platformTable } from "@/db/schemas/linksSchema";
-// import { db } from "@/db";
-// Might have to move to layout (Might not need because its being done in middleware)
-
-// const supabase = createClient()
-// const {data, error} = await (await supabase).auth.getUser()
-// // console.log(data)
-// //
-// if (error || !data?.user){
-//   redirect("/login")
-// }
-
-// TESTING
-// const platforms = await db.select().from(platformTable)
-// console.log(platforms)
+import { LinksSection, SaveButton } from "@/components";
 
 export default function Home() {
   //
